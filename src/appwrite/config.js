4985f9e3eead3ec8dc772029e701d3088b7ae9ef
hi-abhishek12 <abhisahni2005@gmail.com>
2025-04-09 15:19:16 +0000

    appwrite

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -14,8 +14,8 @@ class Service{
      this.bucket = new Storage(this.client)
     }
     
-     
-    async createPost({title , slug , content , status , featuredImage , userId}){
+    
+   async createPost({title , slug , content , status , featuredImage , userId}){
         try {
             return await this.databases.createDocument(
                 conf.appwriteDatabaseId, conf.appwriteCollectionId , slug , {
@@ -26,11 +26,49 @@ class Service{
                     userId
             })
         } catch (error) {
-            
+            console.log('error',error)
         }
    } 
-}
 
+   async updatePost(slug , {title , content , status , featuredImage }){
+    try {
+        return await this.databases.updateDocument(
+            conf.appwriteDatabaseId , conf.appwriteCollectionId, slug ,{
+                title,
+                content,
+                status,
+                featuredImage 
+            }
+        )
+    } catch (error) {
+        throw error;
+    }
+   }
+   
+   async deletePost(slug){
+    try {
+        await this.databases.deleteDocument(
+            conf.appwriteDatabaseId , conf.appwriteCollectionId , slug
+        )
+        return true;
+    } catch (error) {
+        console.log('error',error)
+        return false;
+    }
+   }
+
+   async getPost(slug){
+    try {
+         await this.databases.getDocument(
+            conf.appwriteDatabaseId , 
+            conf.appwriteCollectionId , 
+            slug 
+        )
+    } catch (error) {
+        console.log('error',error);
+    }
+   }
+}
 
 const service = new Service();
 export default service;
