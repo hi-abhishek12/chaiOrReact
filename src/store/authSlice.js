c5c0e4314edcf9fe0b5c6ef45c511c04422796bb
hi-abhishek12 <abhisahni2005@gmail.com>
2025-04-10 14:18:52 +0000

    appwrite

---
+++ src/store/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import authservice from "../appwrite/auth";
+
+const initialState = {
+    status : 'false',
+    userData : null
+}
+const authSlice = createSlice({
+    name : "auth",
+    initialState,
+    reducers: {
+        login : (state , action) =>{
+            state.status = true;
+            state.userData = action.payload;
+        },
+        logout: (state) =>{
+            state.status = false;
+            userData = null
+        }
+    }
+})
+export const {login , logout} = authSlice.actions
+export default authSlice.reducer;
