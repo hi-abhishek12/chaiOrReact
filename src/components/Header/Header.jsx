05c176e1713e9fb9ed2c55f4a68e44739fcb3226
hi-abhishek12 <abhisahni2005@gmail.com>
2025-04-11 14:45:35 +0000

    appwrite

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,8 +11,51 @@ function Header() {
     )
     const navigate = useNavigate()
 
+    const navItems = [
+      {
+        name : 'Home',
+        slug : '/',
+        active : true
+      },
+      {
+        name: "Login",
+        slug: "/login",
+        active: !authStatus,
+    },
+    {
+        name: "Signup",
+        slug: "/signup",
+        active: !authStatus,
+    },
+    {
+        name: "All Posts",
+        slug: "/all-posts",
+        active: authStatus,
+    },
+    {
+        name: "Add Post",
+        slug: "/add-post",
+        active: authStatus,
+    },
+
+    ]
+
   return (
-    <div>Header</div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav>
+          <div className='mr-4'>
+            <Link to ="/">
+              <Logo width='100px'/>
+            </Link>
+          </div>
+            <ul>
+              
+            </ul>
+
+        </nav>
+      </Container>
+    </header>
   )
 }
 
