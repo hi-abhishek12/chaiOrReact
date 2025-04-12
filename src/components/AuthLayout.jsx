b1501d34499dc301da422108980c67a555c6239c
hi-abhishek12 <abhisahni2005@gmail.com>
2025-04-12 17:23:46 +0000

    appwrite

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import React , {useState , useEffect}from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-function AuthLayout() {
+export default function Protected({
+    children , authentication = true
+}) {
+    const navigate = useNavigate();
+    const [loader , setLoader] = useState(true);
+    const authStatus = useSelector((state) => state.auth.status);
+
+    useEffect(() =>{
+        
+    },[authStatus , navigate , authentication])
   return (
     <div>AuthLayout</div>
   )
 }
 
-export default AuthLayout
