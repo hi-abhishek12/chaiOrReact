e39db77a9ff829b68ec3148f209dfcfbe37c2044
hi-abhishek12 <abhisahni2005@gmail.com>
2025-04-27 11:43:07 +0530

    appwrite

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,16 +5,19 @@ import { Link } from 'react-router-dom'
 function PostCard({$id , title , featuredImage}) {
   return (
     <Link to = {`/post/${$id}`}>
-        <div className="w-full bg-gray-50 rounded-2xl p-5 shadow-md hover:shadow-lg transition-shadow duration-300">
+       <div className="w-full bg-gray-50 rounded-2xl p-5 shadow-md hover:shadow-lg transition-shadow duration-300">
   <div className="w-full flex justify-center mb-5">
-    <img
-      src={appwriteservice.getFilePreview(featuredImage)}
-      alt={title}
-      className="rounded-xl object-cover max-h-60 w-full sm:w-auto"
-    />
+    <div className="w-48 h-48 overflow-hidden rounded-xl">
+      <img
+        src={appwriteservice.getFilePreview(featuredImage)}
+        alt={title}
+        className="w-full h-full object-cover"
+      />
+    </div>
   </div>
   <h2 className="text-2xl font-semibold text-gray-900 truncate">{title}</h2>
 </div>
+
 
     </Link>
   )
